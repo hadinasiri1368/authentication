cef97fdb3d8745e759bc4317216a8c31df721234
h.nasiri <h.nasiri@rayanhamafza.com>
2024-11-04 11:53:25 +0330

    Captcha

--- src/main/java/org/authentication/common/Const.java
+++ src/main/java/org/authentication/common/Const.java
@@ -4,4 +4,6 @@ public class Const {
     public final static Integer USER_TYPE_CUSTOMER = 1;
     public final static Integer USER_TYPE_DRIVER = 2;
     public final static Integer USER_TYPE_EMPLOYEE = 3;
+
+    public final static String PROFILE_DEVELOPER_NAME = "dev";
 }
--- src/main/java/org/authentication/dto/ResponseDto/CaptchaData.java
+++ src/main/java/org/authentication/dto/ResponseDto/CaptchaData.java
@@ -1,6 +1,18 @@
 package org.authentication.dto.ResponseDto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import lombok.*;
+import org.authentication.common.Const;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 @Getter
 @Setter
@@ -10,6 +22,31 @@ import lombok.*;
 public class CaptchaData {
     private String captchaToken;
     private String uuid;
+    @JsonSerialize(using = CaptchaCodeSerializer.class)
     private String captchaCode;
     private byte[] image;
+}
+
+@Component
+class CaptchaCodeSerializer extends StdSerializer<String> {
+
+    @Autowired
+    private Environment environment;
+
+    public CaptchaCodeSerializer() {
+        this(null);
+    }
+
+    public CaptchaCodeSerializer(Class<String> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(String value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        if (Arrays.stream(environment.getActiveProfiles()).anyMatch(Const.PROFILE_DEVELOPER_NAME::equals)) {
+            gen.writeString(value);
+        } else {
+            gen.writeNull();
+        }
+    }
 }
